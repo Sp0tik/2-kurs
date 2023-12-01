0fa2abad55230728dfffb0c8a1e8e1efe4713c30
Yura Volynets <yuriyvolinets5@gmail.com>
2023-12-01 18:00:09 +0200

    Final Project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    keyword = models.CharField(max_length=255)
+    publication = models.CharField(max_length=255)
+    resource = models.CharField(max_length=255)
+    content = models.TextField()
+    date_published = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} - {self.keyword}"
+        
